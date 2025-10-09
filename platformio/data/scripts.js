3da62f7dad5005a1638354048361071f55bc2638
DNAMcKnight <conner.cmk9040@gmail.com>
2025-10-09 15:36:28 +0600

    updated frontend callbacks

--- platformio/data/scripts.js
+++ platformio/data/scripts.js
@@ -1,9 +1,31 @@
-const btn = document.getElementById('toggleBtn');
-btn.addEventListener('click', () => {
+const toggle_ups = document.getElementById('toggle_ups');
+toggle_ups.addEventListener('click', () => {
     fetch('/toggle')
         .then(response => {
             if (response.ok) {
-                btn.classList.toggle('active');
+                toggle_ups.classList.toggle('active');
+            }
+        });
+});
+
+
+const complete_shutdown = document.getElementById('complete_shutdown');
+complete_shutdown.addEventListener('click', () => {
+    console.log("Clicked");
+    fetch('/complete_shutdown')
+        .then(response => {
+            if (response.ok) {
+                complete_shutdown.classList.toggle('active');
+            }
+        });
+});
+
+const shutdown = document.getElementById('shutdown');
+shutdown.addEventListener('click', () => {
+    fetch('/shutdown')
+        .then(response => {
+            if (response.ok) {
+                complete_shutdown.classList.toggle('active');
             }
         });
 });
