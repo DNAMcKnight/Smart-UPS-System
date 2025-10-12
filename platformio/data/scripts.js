3f08e6236e35607102321633eb8af76d64c4db4a
DNAMcKnight <conner.cmk9040@gmail.com>
2025-10-13 02:43:30 +0600

    added better error handling and disabled the buttons while request is processing.

--- platformio/data/scripts.js
+++ platformio/data/scripts.js
@@ -1,31 +1,56 @@
-const toggle_ups = document.getElementById('toggle_ups');
-toggle_ups.addEventListener('click', () => {
-    fetch('/toggle')
-        .then(response => {
-            if (response.ok) {
-                toggle_ups.classList.toggle('active');
-            }
-        });
-});
-
-
 const complete_shutdown = document.getElementById('complete_shutdown');
-complete_shutdown.addEventListener('click', () => {
-    console.log("Clicked");
-    fetch('/complete_shutdown')
-        .then(response => {
-            if (response.ok) {
-                complete_shutdown.classList.toggle('active');
-            }
-        });
+const toggle_ups = document.getElementById('toggle_ups');
+const pc_shutdown = document.getElementById('pc_shutdown');
+const toggleBtn3 = document.getElementById('toggleBtn3');
+
+document.addEventListener('DOMContentLoaded', () => { });
+toggle_ups.addEventListener('click', async () => {
+    toggle_ups.disabled = true;
+    try {
+        const response = await fetch('/toggle')
+        if (response.ok) {
+            toggle_ups.classList.toggle('active');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+    finally {
+        toggle_ups.disabled = false;
+    }
+
 });
 
-const shutdown = document.getElementById('shutdown');
-shutdown.addEventListener('click', () => {
-    fetch('/shutdown')
-        .then(response => {
-            if (response.ok) {
-                complete_shutdown.classList.toggle('active');
-            }
-        });
+
+
+complete_shutdown.addEventListener('click', async () => {
+    complete_shutdown.disabled = true;
+    try {
+        const response = await fetch('/complete_shutdown')
+        if (response.ok) {
+            complete_shutdown.classList.toggle('active');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        complete_shutdown.disabled = false;
+    }
 });
+
+
+pc_shutdown.addEventListener('click', async () => {
+    pc_shutdown.disabled = true;
+    try {
+        const response = await fetch('/pc_shutdown')
+        if (response.ok) {
+            pc_shutdown.classList.toggle('active');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        pc_shutdown.disabled = false;
+    }
+});
+
+
+
+toggleBtn3.disabled = true; // Currently we have no use for this button
