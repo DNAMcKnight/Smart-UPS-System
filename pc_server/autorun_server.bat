3ec5af44940f016fd0af7c5c15a5938476da4162
DNAMcKnight <conner.cmk9040@gmail.com>
2026-03-10 14:40:16 +0600

    Updated autorun file

--- pc_server/autorun_server.bat
+++ pc_server/autorun_server.bat
@@ -2,5 +2,5 @@
 cd /d E:\development\ESP8266\Smart-UPS-System\pc_server
 REM Activate the virtual environment and run the server using its Python
 call venv\Scripts\activate.bat
-python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+python -m uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1
 pause
